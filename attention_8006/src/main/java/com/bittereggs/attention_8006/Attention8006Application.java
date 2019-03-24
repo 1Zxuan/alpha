b29d9dade7d445fe599d3e8c00c4c0696c33b0c3
楼中煊 <64390357@qq.com>
2019-03-24 09:38:20 +0800

    update

---
+++ attention_8006/src/main/java/com/bittereggs/attention_8006/Attention8006Application.java
@@ -0,0 +1,13 @@
+package com.bittereggs.attention_8006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Attention8006Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Attention8006Application.class, args);
+    }
+
+}
