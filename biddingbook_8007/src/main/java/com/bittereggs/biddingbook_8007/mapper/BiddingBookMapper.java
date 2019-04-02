7830d2b898ce2015e835be7829324c9b7c6b19a0
1104935833 <1104935833@qq.com>
2019-04-02 17:28:31 +0800

    h3

--- biddingbook_8007/src/main/java/com/bittereggs/biddingbook_8007/mapper/BiddingBookMapper.java
+++ biddingbook_8007/src/main/java/com/bittereggs/biddingbook_8007/mapper/BiddingBookMapper.java
@@ -2,6 +2,8 @@ package com.bittereggs.biddingbook_8007.mapper;
 
 import com.bittereggs.biddingbook_8007.entity.BiddingBook;
 import com.bittereggs.biddingbook_8007.entity.Invatation;
+import com.bittereggs.biddingbook_8007.entity.Phase;
+import com.bittereggs.biddingbook_8007.entity.Tender_info;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +17,29 @@ public interface BiddingBookMapper {
 
     List<BiddingBook> getNoBiddingBook(BiddingBook biddingBook);
 
-    int releaseBiddingBook(BiddingBook biddingBook);
+    void releaseBiddingBook(BiddingBook biddingBook);
 
     int invatationWorkroom(Invatation invatation);
+
+    List<Tender_info> getWinBiddingBook(String workroomusername);
+
+    int initiateBidding(Tender_info tender_info);
+
+    String findWorkroom(String workroom_username,String tender_id);
+
+    int upinitiateBidding(Tender_info tender_info);
+
+    Invatation getWorkroomIniate(String workroomname);
+
+    List<Tender_info> getCompanyBidd(String biddingbookid);
+
+    void releasePhase(BiddingBook biddingBook);
+
+    String confirmOrder(Phase phase);
+
+    Phase getphase(String biddingbookid);
+
+    Phase getPhase(String biddingbookid);
+
+    void passPhase(String document, String picture,String biddingbookid);
 }
--- biddingbook_8007/src/main/java/com/bittereggs/biddingbook_8007/service/BiddingBookService.java
+++ biddingbook_8007/src/main/java/com/bittereggs/biddingbook_8007/service/BiddingBookService.java
@@ -2,6 +2,8 @@ package com.bittereggs.biddingbook_8007.service;
 
 import com.bittereggs.biddingbook_8007.entity.BiddingBook;
 import com.bittereggs.biddingbook_8007.entity.Invatation;
+import com.bittereggs.biddingbook_8007.entity.Phase;
+import com.bittereggs.biddingbook_8007.entity.Tender_info;
 
 
 public interface BiddingBookService {
@@ -12,4 +14,18 @@ public interface BiddingBookService {
     String getNoBiddingBook(BiddingBook biddingBook);
 
     String invatationWorkroom(Invatation invatation);
+
+    String getWinBiddingBook(String workroomusername);
+
+    String initiateBidding(Tender_info tender_info);
+
+    String getWorkroomIniate(String workroomname);
+
+    String getCompanyBidd(String biddingbookid);
+
+    String confirmOrder(Phase phase);
+
+    String getPhase(String biddingbookid);
+
+    String passPhase(String document, String picture,String biddingbookid);
 }
